addba5be32e271e3c6dd801042b54c2f89df36ca
Marcus Vinicius Bianchi dos Santos <marcuss@spi.local>
2018-01-26 17:50:11 -0200

    Correction for Production

--- Program.cs
+++ Program.cs
@@ -8,18 +8,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace statusservice
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+namespace statusservice {
+    public class Program {
+        public static void Main (string[] args) {
+            BuildWebHost (args).Run ();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHost BuildWebHost (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseUrls ("http://*:5004")
+            .UseStartup<Startup> ()
+            .Build ();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -15,56 +16,49 @@ using statusservice.Data;
 using statusservice.Services;
 using statusservice.Services.Interfaces;
 
-namespace statusservice
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace statusservice {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                                .AllowAnyMethod()
-                                .AllowAnyHeader();
-                    }));
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("StatusDb")));
-            services.AddSingleton<IThingService, ThingService>();
-            services.AddTransient<IThingStatusService, ThingStatusService>();
-            services.AddTransient<IContextStatusService, ContextStatusService>();
-            services.AddResponseCaching();
-            services.AddMvc((options) =>
-            {
-                options.CacheProfiles.Add("statuscache", new CacheProfile()
-                {
-                    Duration = Convert.ToInt32(Configuration["CacheDuration"]),
-                    Location = ResponseCacheLocation.Any
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddCors (o => o.AddPolicy ("CorsPolicy", builder => {
+                builder.AllowAnyOrigin ()
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ();
+            }));
+            services.AddDbContext<ApplicationDbContext> (options =>
+                options.UseNpgsql (Configuration.GetConnectionString ("StatusDb")));
+            services.AddSingleton<IThingService, ThingService> ();
+            services.AddTransient<IThingStatusService, ThingStatusService> ();
+            services.AddTransient<IContextStatusService, ContextStatusService> ();
+            services.AddResponseCaching ();
+            services.AddMvc ((options) => {
+                options.CacheProfiles.Add ("statuscache", new CacheProfile () {
+                    Duration = Convert.ToInt32 (Configuration["CacheDuration"]),
+                        Location = ResponseCacheLocation.Any
                 });
-            }).AddJsonOptions(options =>
-            {
+            }).AddJsonOptions (options => {
                 options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseCors("CorsPolicy");
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseResponseCaching ();
+            app.UseCors ("CorsPolicy");
+            app.UseForwardedHeaders (new ForwardedHeadersOptions {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseMvc();
+            app.UseMvc ();
         }
     }
 }
